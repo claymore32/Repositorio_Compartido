5a926fa0516bbcec915f3a8a1711c3c3eea7859f
bastiancoronado <sebastian.coronado@upb.edu.co>
2021-04-18 18:17:14 -0500

    Creacion de las tareas en el proyecto
    Descripcion

---
+++ compartido/compartido.ino
@@ -0,0 +1,17 @@
+ void setup() {
+   Serial.begin(115200);
+ }
+
+void loop() {
+  task_sebasJ();
+  task_sebasC();
+}
+
+void task_sebasJ(){
+  
+}
+
+void task_sebasC(){
+  Serial.println("Hola");
+  delay(3000);
+}
